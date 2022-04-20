16d9528664b22cde29e89d8af3d54d1b610c3657
Hai Duong <88724391+HDuong1803@users.noreply.github.com>
2022-04-20 12:50:40 +0700

    Success

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import './App.css';
+import MainMint from './MainMint';
+import NavBar from './NavBar';
+
+function App() {
+  const [accounts, setAccounts] = useState([]);
+
+  return (
+    <div className='ovelay'>
+      <div className="App">
+        <NavBar accounts={accounts} setAccounts={setAccounts} />
+        <MainMint accounts={accounts} setAccounts={setAccounts} />
+      </div>
+      <div className='moving-background'></div>
+    </div>
+  );
+}
+
+export default App;
